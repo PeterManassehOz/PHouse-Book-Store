8981134613e717a197ddd7efbc3bda36ebf9e67f
chibuezeozojideofor@gmail.com <Peter_Manasseh_Oz>
2025-06-25 14:30:32 +0100

    Updated the useUserProfile hook

--- client/src/hooks/useUserProfile.js
+++ client/src/hooks/useUserProfile.js
@@ -1,6 +1,16 @@
 import { useGetUserProfileQuery } from "../redux/profileAuthApi/profileAuthApi";
 import profileIcon from "/profileIconBrown.jpeg";
 
+
+
+// read the env var
+const API_BASE = import.meta.env.VITE_API_BASE;
+
+// log it so you know what the running code is actually using
+console.log('🛰️ API_BASE is:', API_BASE);
+
+
+
 const useUserProfile = () => {
   // Fetch user profile
   const { data: userProfile, refetch, isLoading, error } = useGetUserProfileQuery(undefined, {
@@ -11,7 +21,7 @@ const useUserProfile = () => {
 
   if (userProfile?.image) {
     const timestamp = new Date().getTime();
-    profileImageUrl = `http://localhost:5000/uploads/${userProfile.image.split("/").pop()}?t=${timestamp}`;
+    profileImageUrl = `${API_BASE}/uploads/${userProfile.image.split("/").pop()}?t=${timestamp}`;
   }
 
   return { userProfile, profileImageUrl, refetch, isLoading, error };
